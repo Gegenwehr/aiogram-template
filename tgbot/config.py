e030e5e24d26fa8715122e1a7dc1377946cb7ad7
The greattest gegenwehr <91827114+Gegenwehr@users.noreply.github.com>
2023-01-10 10:12:59 +0300

    Update config.py

--- tgbot/config.py
+++ tgbot/config.py
@@ -30,21 +30,23 @@ class Config:
     misc: Miscellaneous
 
 
-def load_config(path: str = None):
+def load_config(path: str = None, db: bool = False):
     env = Env()
     env.read_env(path)
 
-    return Config(
-        tg_bot=TgBot(
-            token=env.str("BOT_TOKEN"),
-            admin_ids=list(map(int, env.list("ADMINS"))),
-            use_redis=env.bool("USE_REDIS"),
-        ),
-        db=DbConfig(
+    if db:
+        return DbConfig(
             host=env.str('DB_HOST'),
             password=env.str('DB_PASS'),
             user=env.str('DB_USER'),
             database=env.str('DB_NAME')
-        ),
-        misc=Miscellaneous()
-    )
+        )
+    else:
+        return Config(
+            tg_bot=TgBot(
+                token=env.str("BOT_TOKEN"),
+                admin_ids=list(map(int, env.list("ADMINS"))),
+                use_redis=env.bool("USE_REDIS"),
+            ),
+            misc=Miscellaneous()
+        )
