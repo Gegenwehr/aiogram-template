c9829f2bae79b0adbcc4279d83c659cbb170973b
storm <s@s.ru>
2023-01-10 09:31:59 +0300

    All done

--- bot.py
+++ bot.py
@@ -6,9 +6,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from tgbot.config import load_config
-from tgbot.filters.admin import AdminFilter
+from tgbot.filters import AdminFilter
 from tgbot.handlers.admin import register_admin
-from tgbot.handlers.echo import register_echo
 from tgbot.handlers.user import register_user
 from tgbot.middlewares import DbMiddleware
 
